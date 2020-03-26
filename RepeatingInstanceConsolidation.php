c97b9453a497b7a1eac4a2d6cb1fd8dd53b18d4e
Kyle McGuffin <kyle.mcguffin@vumc.org>
2020-03-26 12:16:53 -0400

    Add hook to links (add record ID), so we can have a reconciliation link for individual records Add hook to allow saving configuration in JSON or dropdown format

--- RepeatingInstanceConsolidation.php
+++ RepeatingInstanceConsolidation.php
@@ -7,5 +7,105 @@ class RepeatingInstanceConsolidation extends \ExternalModules\AbstractExternalMo
 
 
 	}
-	
+
+	public function refactorDropdownsToJson($newForms,$newTypes,$newFields,$newMatchingFields) {
+		$combinedJson = ["input" => [],"reconciled" => [], "output" => []];
+
+		## Combine the separate fields so they can be compared to the JSON version
+		foreach($newForms as $inputKey => $formName) {
+			$type = $newTypes[$inputKey];
+			$newRow = [];
+
+			if(in_array($type,["input","reconciled"])) {
+				$newRow = ["matching" => $newMatchingFields[$inputKey], "fields" => $newFields[$inputKey]];
+			}
+			else if($type == "output") {
+				$newRow = ["fields" => $newFields[$inputKey]];
+			}
+			else {
+				continue;
+			}
+
+			$combinedJson[$type][$formName] = $newRow;
+		}
+
+		$combinedJson = json_encode($combinedJson);
+
+		return $combinedJson;
+	}
+
+	public function redcap_module_save_configuration( $project_id ) {
+		$oldJson = $this->getProjectSetting('existing-json');
+		$newJson = $this->getProjectSetting('input-json');
+		$newForms = $this->getProjectSetting('input-forms');
+		$newTypes = $this->getProjectSetting('input-types');
+		$newFields = $this->getProjectSetting('input-fields');
+		$newMatchingFields = $this->getProjectSetting('input-matching-fields');
+
+		$combinedJson = $this->refactorDropdownsToJson($newForms,$newTypes,$newFields,$newMatchingFields);
+		$tempJson = json_decode($newJson, true);
+
+		$updateFromCombined = $combinedJson != $oldJson;
+		$updateFromJson = $newJson != $oldJson;
+
+		if($_SERVER['SERVER_NAME'] == "localhost") {
+			error_log("Instance: $updateFromJson ~ $updateFromCombined ~ $combinedJson");
+		}
+
+		if($updateFromJson) {
+			if($tempJson !== NULL) {
+				## Update the dropdowns from JSON because it's valid JSON
+				$updatedForms = [];
+				$updatedTypes = [];
+				$updatedFields = [];
+				$updatedMatchingFields = [];
+
+				foreach($tempJson as $type => $typeDetails) {
+					foreach($typeDetails as $formName => $formDetails) {
+						if(count($formDetails["fields"]) == 0) {
+							$formDetails["fields"] = [null];
+						}
+						if(count($formDetails["matching"]) == 0) {
+							$formDetails["matching"] = [null];
+						}
+
+						$updatedForms[] = $formName;
+						$updatedTypes[] = $type;
+						$updatedFields[] = $formDetails["fields"];
+						$updatedMatchingFields[] = $formDetails["matching"];
+					}
+				}
+
+				$this->setProjectSetting('input-forms',$updatedForms);
+				$this->setProjectSetting('input-types',$updatedTypes);
+				$this->setProjectSetting('input-fields',$updatedFields);
+				$this->setProjectSetting('input-matching-fields',$updatedMatchingFields);
+				$this->setProjectSetting('form-settings',array_fill(0,count($updatedForms),"true"));
+
+				## Refactor JSON so it matches the next time it's re-saved
+				$newJson = $this->refactorDropdownsToJson($updatedForms,$updatedTypes,$updatedFields,$updatedMatchingFields);
+
+				$this->setProjectSetting("input-json",$newJson);
+				$this->setProjectSetting("existing-json",$newJson);
+			}
+		}
+		else if($updateFromCombined) {
+			## Update the actual JSON and the input JSON fields from the dropdown values
+			$this->setProjectSetting('input-json',$combinedJson);
+			$this->setProjectSetting('existing-json',$combinedJson);
+		}
+	}
+
+	public function redcap_module_link_check_display( $project_id, $link ) {
+		if($link['name'] == "Reconcile Data") {
+			if(!empty($_GET['id'])) {
+				$link['url'] = $link['url']."&record=".$_GET['id'];
+			}
+			else {
+				return false;
+			}
+		}
+
+		return parent::redcap_module_link_check_display($project_id,$link);
+	}
 }
