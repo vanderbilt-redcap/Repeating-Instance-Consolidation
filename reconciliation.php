10e61d46dc8ea5f472e272335deae02824665dc4
Chad Lightner <chad@chadlightner.com>
2020-04-01 17:50:22 -0500

    Update reconciliation.php

--- reconciliation.php
+++ reconciliation.php
@@ -144,10 +144,10 @@ $c_column = 0;
 		foreach($formDetails as $instanceId => $instanceDetails) {
 			//echo "<tr><td class='fcol'>";
 
-			$frmName = echo str_replace("_"," ",$formName);
-			$frmName = echo str_replace("recipient","",$formName);
-			$frmName = echo str_replace("blood","bld",$formName);
-			$frmName = echo str_replace("single","sgle",$formName);
+			$frmName =  str_replace("_"," ",$formName);
+			$frmName =  str_replace("recipient","",$formName);
+			$frmName =  str_replace("blood","bld",$formName);
+			$frmName =  str_replace("single","sgle",$formName);
 			echo "<tr><td class='fcol'>$frmName : Instance $instanceId - ";
 
 			$matchedData = explode("~",$matchingValue);
