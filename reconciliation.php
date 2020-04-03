e231ae1031ddf0820a130c4ec3c3b785b1b836a8
Chad Lightner <chad@chadlightner.com>
2020-04-03 17:35:31 -0500

    Update reconciliation.php

--- reconciliation.php
+++ reconciliation.php
@@ -261,7 +261,7 @@ jQuery(document).ready(function($){
 		}
 		var offset = $(this).offset();
 		var fcolwidth = $(".wdgmctable>thead>tr>th:first-of-type").width();
-		$(this).before("<div class='hdrback "+addtclass+"' style='position:absolute; left:" + (offset.left-fcolwidth+20) + "px; top:0px; height:"+$(".wdgmctable").height()+"px; width:"+$(this).width()+"px; z-index: -1;'></div>");
+		$(this).before("<div class='hdrback "+addtclass+"' style='position:absolute; left:" + (offset.left-fcolwidth) + "px; top:0px; height:"+$(".wdgmctable").height()+"px; width:"+$(this).width()+"px; z-index: -1;'></div>");
 	});
 });
 
