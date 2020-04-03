c2475eeb19332c862e497ef83e44f81e9e624799
Kyle McGuffin <kyle.mcguffin@vumc.org>
2020-04-03 10:28:16 -0400

    Get the comparison data to actually classify each record on report.php and display a link to the reconciliation page for it

--- report.php
+++ report.php
@@ -15,8 +15,63 @@ $records = REDCap::getData(["project_id" => $module->getProjectId(),"fields" => 
 
 $records = array_keys($records);
 
+$questionableRecords = [];
+$needsReconciliationRecords = [];
+$unreconciledRecords = [];
+
 foreach($records as $thisRecord) {
-	$comparisonData = $module->getComparisonData($thisRecord,$module->getProjectId());
+	$comparisonData = $module->getComparisonData($thisRecord,$module->getProjectId())["comparison"];
+	$confirmedAntibodies = [];
+	$questionableAntibodies = [];
 
+	foreach($comparisonData as $matchingKey => $matchedData) {
+		foreach($matchedData as $fieldKey => $fieldData) {
+			foreach($fieldData as $rawValue => $checkedList) {
+				if(count($checkedList) > 1) {
+					$questionableAntibodies[$fieldKey][$rawValue] = 1;
+				}
+				else if(array_key_exists(1,$checkedList)) {
+					$confirmedAntibodies[$fieldKey][$rawValue] = 1;
+				}
+			}
+		}
+	}
 
+	$hasQuestionableAntibodies = false;
+	$hasUnreconciledIssues = (count($questionableAntibodies) > 0);
+	foreach($questionableAntibodies as $fieldKey => $fieldValues) {
+		foreach($fieldValues as $rawValue => $confirmed) {
+			if(!array_key_exists($rawValue,$confirmedAntibodies[$fieldKey])) {
+				$hasQuestionableAntibodies = true;
+				break 2;
+			}
+		}
+	}
+
+	if($hasQuestionableAntibodies) {
+		$questionableRecords[] = $thisRecord;
+	}
+	else if($hasUnreconciledIssues) {
+		$needsReconciliationRecords[] = $thisRecord;
+	}
+	else {
+		$unreconciledRecords[] = $thisRecord;
+	}
+}
+
+echo "<h3>Records with questionable antibodies</h3>";
+foreach($questionableRecords as $thisRecord) {
+	echo "<a href='".$module->getUrl("reconciliation.php?id=".$thisRecord)."'>Record ".$thisRecord."<br />";
+}
+
+echo "<br /><br />";
+echo "<h3>Records with conflicts</h3>";
+foreach($needsReconciliationRecords as $thisRecord) {
+	echo "<a href='".$module->getUrl("reconciliation.php?id=".$thisRecord)."'>Record ".$thisRecord."<br />";
+}
+
+echo "<br /><br />";
+echo "<h3>Records with un-reviewed tests</h3>";
+foreach($unreconciledRecords as $thisRecord) {
+	echo "<a href='".$module->getUrl("reconciliation.php?id=".$thisRecord)."'>Record ".$thisRecord."<br />";
 }
