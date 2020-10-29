257819c8fdd746e97b74e56af37314c56d311be9
Kyle McGuffin <kyle.mcguffin@vumc.org>
2020-10-29 15:52:06 -0400

    Update labeling on report to show name and DOB Add categories for records with: no data, fully reviewed data, tests that don't have a DD match Implement logic for all 3 new categories

--- report.php
+++ report.php
@@ -11,22 +11,34 @@
 require_once \ExternalModules\ExternalModules::getProjectHeaderPath();
 
 $startTime = microtime(true);
-$records = REDCap::getData(["project_id" => $module->getProjectId(),"fields" => REDCap::getRecordIdField()]);
+$recordData = REDCap::getData(["project_id" => $module->getProjectId(),"fields" => [REDCap::getRecordIdField(),"rec_name","rec_dob"]]);
 
-$records = array_keys($records);
+$records = array_keys($recordData);
 
 $questionableRecords = [];
 $needsReconciliationRecords = [];
 $unreconciledRecords = [];
+$noDataRecords = [];
+$reviewedRecords = [];
+$unmatchedRecords = [];
+$recordDisplay = [];
 
 ## TODO What to do with records that have only one entry for a given test date?
 
 foreach($records as $thisRecord) {
-	$comparisonData = $module->getComparisonData($module->getProjectId(),$thisRecord)["comparison"];
+	$recordDisplay[$thisRecord] = $thisRecord.": ".reset($recordData[$thisRecord])["rec_name"]." - ".
+			reset($recordData[$thisRecord])["rec_dob"];
+
+	$comparisonData = $module->getComparisonData($module->getProjectId(),$thisRecord);
 	$confirmedAntibodies = [];
 	$questionableAntibodies = [];
 
-	foreach($comparisonData as $matchingKey => $matchedData) {
+	if(count($comparisonData["comparison"]) == 0) {
+		$noDataRecords[] = $thisRecord;
+		continue;
+	}
+
+	foreach($comparisonData["comparison"] as $matchingKey => $matchedData) {
 		foreach($matchedData as $fieldKey => $fieldData) {
 			foreach($fieldData as $rawValue => $checkedList) {
 				if(count($checkedList) > 1) {
@@ -50,30 +62,67 @@ foreach($records as $thisRecord) {
 		}
 	}
 
+	$hasUnreconciledTests = false;
+	foreach($comparisonData["combined"]["input"] as $matchingValue => $matchedData) {
+		if(!array_key_exists($matchingValue,$comparisonData["combined"]["reconciled"])) {
+			$hasUnreconciledTests = true;
+			break;
+		}
+	}
+
+
+	foreach($comparisonData["combined"]["input"] as $matchingValue => $matchedData) {
+		if(count($matchedData) <= 1 && count(reset($matchedData)) <= 1) {
+			$unmatchedRecords[] = $thisRecord;
+			break;
+		}
+	}
+
 	if($hasQuestionableAntibodies) {
 		$questionableRecords[] = $thisRecord;
 	}
 	else if($hasUnreconciledIssues) {
 		$needsReconciliationRecords[] = $thisRecord;
 	}
-	else {
+	else if($hasUnreconciledTests){
 		$unreconciledRecords[] = $thisRecord;
+	}
+	else {
+		$reviewedRecords[] = $thisRecord;
 	}
 }
 
 echo "<h3>Records with questionable antibodies</h3>";
 foreach($questionableRecords as $thisRecord) {
-	echo "<a href='".$module->getUrl("reconciliation.php?id=".$thisRecord)."'>Record ".$thisRecord."</a><br />";
+	echo "<a href='".$module->getUrl("reconciliation.php?id=".$thisRecord)."'>".$recordDisplay[$thisRecord]."</a><br />";
 }
 
 echo "<br /><br />";
 echo "<h3>Records with conflicts</h3>";
 foreach($needsReconciliationRecords as $thisRecord) {
-	echo "<a href='".$module->getUrl("reconciliation.php?id=".$thisRecord)."'>Record ".$thisRecord."</a><br />";
+	echo "<a href='".$module->getUrl("reconciliation.php?id=".$thisRecord)."'>".$recordDisplay[$thisRecord]."</a><br />";
 }
 
 echo "<br /><br />";
 echo "<h3>Records with un-reviewed tests</h3>";
 foreach($unreconciledRecords as $thisRecord) {
-	echo "<a href='".$module->getUrl("reconciliation.php?id=".$thisRecord)."'>Record ".$thisRecord."</a><br />";
+	echo "<a href='".$module->getUrl("reconciliation.php?id=".$thisRecord)."'>".$recordDisplay[$thisRecord]."</a><br />";
+}
+
+echo "<br /><br />";
+echo "<h3>Records with un-matching test dates</h3>";
+foreach($unmatchedRecords as $thisRecord) {
+	echo "<a href='".$module->getUrl("reconciliation.php?id=".$thisRecord)."'>".$recordDisplay[$thisRecord]."</a><br />";
+}
+
+echo "<br /><br />";
+echo "<h3>Records with no data</h3>";
+foreach($noDataRecords as $thisRecord) {
+	echo "<a href='".$module->getUrl("reconciliation.php?id=".$thisRecord)."'>".$recordDisplay[$thisRecord]."</a><br />";
+}
+
+echo "<br /><br />";
+echo "<h3>Fully Reviewed Records</h3>";
+foreach($reviewedRecords as $thisRecord) {
+	echo "<a href='".$module->getUrl("reconciliation.php?id=".$thisRecord)."'>".$recordDisplay[$thisRecord]."</a><br />";
 }
